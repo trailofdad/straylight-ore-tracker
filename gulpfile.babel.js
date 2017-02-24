6347a6b04ab2e45aa2ff46d829060ca09e4b1583
chapgood <christian.hapgood@redspace.com>
2017-02-24 15:47:32 -0400

    Remove admin styles task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,4 +3,4 @@ import gulp from 'gulp';
 
 requireDir('./gulp-tasks');
 gulp.task('default', ['watch', 'scripts-common', 'admin-scripts', 'styles', 'lint']);
-gulp.task('build-release', ['scripts-common', 'styles', 'admin-styles']);
+gulp.task('build-release', ['scripts-common', 'styles']);
