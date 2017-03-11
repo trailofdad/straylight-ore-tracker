abee53a0c34558002e09771f27474f50af7ea56c
chapgood <christian.hapgood@redspace.com>
2017-03-11 12:01:28 -0400

    add basic validation and clean up js

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,26 @@
 /* global document */
 /* eslint-disable no-undef */
 $(document).ready(() => {
-  console.log('Amarr Victor');
-  const row = $('.ore-log__wrapper');
-  const container = $('#ore-log-container');
-
-  $('.ore-log__close').click(function close() {
-    $(this).parent().parent().remove();
-  });
-
-  $('#add-ore-type').click(() => {
-    row.clone(true).appendTo(container);
-    $('.ore-log__close').click(function close() {
-      $(this).parent().parent().remove();
+  function validate() {
+    // validate ore values
+    const oreValues = $('.ore-log__input');
+    oreValues.each(function (index) {
+      if (!oreValues[index].validity.valid) {
+        alert('invalid field');
+        return false;
+      }
     });
-  });
+    // validate title & description
+    const title = $('#log-title')[0];
+    const description = $('#log-description')[0];
+    if (!title.validity.valid || !description.validity.valid) {
+      alert('invalid title or description field');
+      return false;
+    }
+    return true;
+  }
 
-  $('#submit-ore-log').click(() => {
+  function submitLog() {
     $('#submit-ore-log').prop('disabled', true);
     // grab the totals
     const oreLogs = $('.ore-log');
@@ -34,25 +38,60 @@ $(document).ready(() => {
       oreData[index] = oreObject;
     });
 
-    $.ajax({
-      type: 'POST',
-      // TODO: make base of this an env variable
-      url: 'http://straylight.dev/wp-json/sot/v1/logs/submit',
-      data: {
-        log_title: logTitle,
-        log_description: logDescription,
-        log_data: oreData,
-        id: wp.id,
-      },
-      success: () => {
-        console.log('Log Submitted Successfully');
-        $('.ore-log__wrapper').remove();
-        $('#submit-ore-log').replaceWith('<h2 style="padding-top:5rem;">Thank you commander, your Ore Log has been submitted.</h2>');
-      },
-      error: () => {
-        console.log('Something has gone terribly wrong with submitting the log');
-      },
-      dataType: 'json',
+    if (validate()) {
+      $.ajax({
+        type: 'POST',
+        // TODO: make base of this an env variable
+        url: 'http://straylight.dev/wp-json/sot/v1/logs/submit',
+        data: {
+          log_title: logTitle,
+          log_description: logDescription,
+          log_data: oreData,
+          id: wp.id,
+        },
+        success: () => {
+          console.log('Log Submitted Successfully');
+          $('.ore-log__wrapper').remove();
+          $('#submit-ore-log').replaceWith('<h2 style="padding-top:5rem;">Thank you commander, your Ore Log has been submitted.</h2>');
+        },
+        error: () => {
+          console.log('Something has gone terribly wrong with submitting the log');
+        },
+        dataType: 'json',
+      });
+    } else {
+      alert('Please ensure you have filled out all fields');
+      $('#submit-ore-log').prop('disabled', false);
+    }
+  }
+
+  function addEventListeners() {
+    const row = $('.ore-log__wrapper');
+    const container = $('#ore-log-container');
+
+    // Remove ore type
+    $('.ore-log__close').click(function close() {
+      $(this).parent().parent().remove();
     });
-  });
+
+    // Add new Ore Type
+    $('#add-ore-type').click(() => {
+      row.clone(true).appendTo(container);
+      $('.ore-log__close').click(function close() {
+        $(this).parent().parent().remove();
+      });
+    });
+
+    // Submit
+    $('#submit-ore-log').click(() => {
+      submitLog();
+    });
+  }
+
+  function init() {
+    console.log('Amarr Victor');
+    addEventListeners();
+  }
+
+  init();
 });
